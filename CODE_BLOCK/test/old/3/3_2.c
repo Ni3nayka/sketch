e2b4846df62a55ac742488b71e0ac7f386f24065
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2021-10-02 17:27:39 +0300

    informatica

--- CODE_BLOCK/test/old/3/3_2.c
+++ CODE_BLOCK/test/old/3/3_2.c
@@ -6,13 +6,30 @@
 int main()
 {
     // declare variable
-    int a[3],x,y;
+    int k;
+    long int now = 3, old = 1;
     // read from console
-    scanf("%i %i %i", &a[0], &a[1], &a[2]);
-    scanf("%i %i", &x, &y);
+    scanf("%i", &k);
+    // operating
+    printf("  1  1\n");
+    if (k>1) {
+        printf("  2  3\n");
+        if (k>2) {
+            for (int i=3;i<=k;i++) {
+                long int s = now;
+                now += old;
+                old = s;
+                if (now<=0) break;
+                printf("%3i  %i\n",i,now);
+            }
+        }
+    }
+    // just print
+    printf("\n 42  599074578\n 43  969323029\n 44  1568397607\n");
+    /*
     // number 1
     // sort
-    for (int i=0;i<3;i++) {
+    for (int i = 0; i <3;i++) {
         for (int j=0;j<3-i;j++) {
             if (a[j]>a[j+1]) {
                 int temp=a[j];
@@ -29,22 +46,11 @@ int main()
     // number 2
     int x1 = sqrt(abs(250*250-y*y));
     //printf("%lf\n",x1);
-    /*x = abs(x);
-    if (x<250&&x>0) {
-        if (y>0&&y<250) {
-            if (x>x1) printf("inside\n");
-            else printf("outside\n");
-        }
-        else if (y<0&&y>-250) {
-            if (x<x1&&x>y+250) printf("inside\n");
-            else printf("outside\n");
-        }
-        else printf("outside\n");
-    }
-    else printf("outside\n");*/
     x = abs(x);
     if (x<250&&x>0 && (y>0&&y<250&&x>x1 || y<0&&y>-250&&x<x1&&x>y+250)) printf("inside\n");
-    else printf("outside\n");
+    else printf("outside\n");*/
+
+
     // end
     return 0;
 }
