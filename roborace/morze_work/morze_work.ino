abd1cdcec35570b2af1f03b7657f7a92131a19a5
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2021-10-16 19:54:20 +0300

    +

---
+++ roborace/morze_work/morze_work.ino
@@ -0,0 +1,14 @@
+// подключаем файл с константами
+#include "constant.h"
+
+void setup() {
+  speak(azbuka[0]); // передаем в процедуру букву 'А'
+}
+
+void loop() {
+  
+}
+
+void speak(int mas[mas_size]) {
+   // если это буква 'А', то: mas = {0,1,2,0,0,0,0}
+}
