bb1fca2de1bc90dc63940e3bf6c65428bffdde80
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2022-08-08 18:36:01 +0300

    Update SERVER_MAIN.py

--- SERVER/SERVER_MAIN.py
+++ SERVER/SERVER_MAIN.py
@@ -27,6 +27,14 @@ class StartServer(Thread):
     def __init__(self,python_path): 
         Thread.__init__(self)
         self.python_path = "python " + python_path
+        self.python_path_folder = python_path
+        #self.disk = python_path[0:2]
+        i = len(self.python_path_folder)-1
+        while self.python_path_folder[i]!='/' and self.python_path_folder[i]!=chr(92):
+            i-=1
+        self.python_main = "python " + self.python_path_folder[(i+1):]
+        self.python_path_folder = self.python_path_folder[:i]
+        #print(self.python_main)
         self.timer = 0
     
     def run(self): 
@@ -35,7 +43,9 @@ class StartServer(Thread):
             if self.timer!=0:
                 print("ERROR and restart: " + self.python_path)
             self.timer = time()
-            subprocess.run(self.python_path)
+            #subprocess.run(self.python_path)
+            subprocess.run(self.python_main,cwd=self.python_path_folder)
+            
         print("FATAL ERROR: " + self.python_path)
 
 # file = open(str(os.getcwd())+"D:/SERVER/config.txt", "r")
