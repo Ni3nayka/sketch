422bbfc6d910af9b13905420d4c21911fa794e4a
Egor Bakay <egor_bakay@inbox.ru>
2022-09-11 22:59:56 +0300

    Update roborace_block_2022.ino

--- olymp/robofinist_2022/Egor/roborace_block_2022/roborace_block_2022.ino
+++ olymp/robofinist_2022/Egor/roborace_block_2022/roborace_block_2022.ino
@@ -6,8 +6,9 @@ AF_DCMotor motor_R(2);
 #define DAT_L A1
 #define DAT_R A2
 
-#define basic_speed 45
-#define Ke 0.3
+#define basic_speed 60
+#define turn_speed 70
+#define Ke 0.4
 
 unsigned long int time = 0;
 
@@ -17,7 +18,7 @@ void setup() {
   motor_R.run(FORWARD);
 }
 void loop () { 
-  int e = analogRead(DAT_L) - analogRead(DAT_R) - 36;
+  int e = analogRead(DAT_L) - analogRead(DAT_R);
   e *= Ke;
   motor_L.setSpeed(constrain(basic_speed + e,0,255));
   motor_R.setSpeed(constrain(basic_speed - e,0,255));
@@ -25,8 +26,8 @@ void loop () {
 
   if (time+3000<millis()) {
     motor_L.run(BACKWARD);
-    motor_L.setSpeed(basic_speed);
-    motor_R.setSpeed(basic_speed);
+    motor_L.setSpeed(turn_speed);
+    motor_R.setSpeed(turn_speed);
     while (analogRead(DAT_L)<850); delay(50);
     while (analogRead(DAT_L)>850); delay(50);
     while (analogRead(DAT_L)<850); delay(50);
